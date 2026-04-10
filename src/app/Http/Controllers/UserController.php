76ee552be0da51d0bb5701178fd0adbd36eebeaf
yoko_sakamaki <androidpug@gmail.com>
2026-04-10 12:51:08 +0900

    fix: mypageの関連ファイルを復元

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProfileRequest;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -9,27 +10,36 @@ class UserController extends Controller
     public function index()
     {
         $user = auth()->user();
+        $sellingItems = $user->items()->where('is_sold', false)->get();
+        $purchasedItems = $user->purchases()->with('item')->get();
 
-        return view('user.mypage', compact('user'));
+        return view('user.mypage', compact('user', 'sellingItems', 'purchasedItems'));
     }
 
     public function edit()
     {
         $user = auth()->user();
-
         return view('user.profile', compact('user'));
     }
 
-    public function update(Request $request)
+    public function update(ProfileRequest $request)
     {
         $user = auth()->user();
 
-        $user->update([
+        $data = [
             'name' => $request->name,
             'postal_code' => $request->postal_code,
             'address' => $request->address,
             'building' => $request->building,
-        ]);
+        ];
+
+        if ($request->hasFile('profile_image')) {
+            $path = $request->file('profile_image')
+                ->store('profile_images', 'public');
+            $data['profile_image'] = $path;
+        }
+
+        $user->update($data);
 
         return redirect('/mypage');
     }
